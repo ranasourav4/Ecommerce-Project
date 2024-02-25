966f40e6776ba8c74416fbcb8c619516ce868d53
ranasourav4 <161185162+ranasourav4@users.noreply.github.com>
2024-02-25 10:58:31 +0530

    Add files via upload

---
+++ CoverType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Ecommerce_29.Models
+{
+    public class CoverType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
