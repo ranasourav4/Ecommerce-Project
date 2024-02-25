671dc1443b9f0f64265f223ae48410c9539d2bc4
ranasourav4 <161185162+ranasourav4@users.noreply.github.com>
2024-02-25 10:59:01 +0530

    Add files via upload

---
+++ SD.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Ecommerce_29.Utility
+{
+    public static class SD
+    {
+        //Roles
+        public const string Role_Admin = "Admin";
+        public const string Role_Employee = "Employee User";
+        public const string Role_Company = "Company User";
+        public const string Role_Individual = "Individual User";
+
+        //Order Status
+        public const string OrderStatusPending = "Pending";
+        public const string OrderStatusApproved = "Approved";
+        public const string OrderStatusProcessing = "Processing";
+        public const string OrderStatusShipped = "Shipped";
+        public const string OrderStatusCancelled = "Cancelled";
+        public const string OrderStatusRefunded = "Refunded";
+
+        //Payment Status
+        public const string PaymentStatusPending = "Pending";
+        public const string PaymentStatusApproved = "Approved";
+        public const string PaymentStatusDelayPayment = "PaymentStatusDelay";
+        public const string PaymentStatusRejected = "Rejected";
+
+        //Session
+        public const string Ss_CartSessionCount = "Cart Count Session";
+
+        public static double GetPriceBasedQuantity(double quantity,double price,double price50, double price100)
+        {
+            if(quantity < 50)
+                return price;
+            else if(quantity <100)
+                return price50;
+            else return price100;
+        }
+    }
+}
---
+++ StripeSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Ecommerce_29.Utility
+{
+    public class StripeSettings
+    {
+        public string PublishableKey { get; set; }
+        public string Secretkey { get; set; }
+    }
+}
